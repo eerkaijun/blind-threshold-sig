c5ca4ce7fc42a85dcf0ce3d8c495d577b97d724a
bing <spiralladder@keemail.me>
2025-06-21 11:32:32 +0200

    ciphersuite: implement hash functions H1, H2, H4, H5

---
+++ src/ciphersuite.rs
@@ -0,0 +1,45 @@
+use sha2::{Digest, Sha512};
+
+pub const CONTEXT_STRING: &'static str = "FROST-ED25519-SHA512-v1";
+
+pub fn H1(m: Vec<u8>) -> Vec<u8> {
+    let mut hasher = Sha512::new();
+
+    hasher.update(CONTEXT_STRING);
+    hasher.update(b"rho");
+    hasher.update(m);
+
+    let output = hasher.finalize();
+    return output.to_vec();
+}
+
+pub fn H2(m: Vec<u8>) -> Vec<u8> {
+    let mut hasher = Sha512::new();
+
+    hasher.update(m);
+
+    let output = hasher.finalize();
+    return output.to_vec();
+}
+
+pub fn H4(m: Vec<u8>) -> Vec<u8> {
+    let mut hasher = Sha512::new();
+
+    hasher.update(CONTEXT_STRING);
+    hasher.update(b"msg");
+    hasher.update(m);
+
+    let output = hasher.finalize();
+    return output.to_vec();
+}
+
+pub fn H5(m: Vec<u8>) -> Vec<u8> {
+    let mut hasher = Sha512::new();
+
+    hasher.update(CONTEXT_STRING);
+    hasher.update(b"com");
+    hasher.update(m);
+
+    let output = hasher.finalize();
+    return output.to_vec();
+}
