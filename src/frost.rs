8fb7535b5ea9b1870ac54e257831f85070a0f50c
eerkaijun <kaijuneer@gmail.com>
2025-06-21 15:37:21 +0200

    frost scaffolding

---
+++ src/frost.rs
@@ -0,0 +1,25 @@
+use ark_ed25519::Fr as ScalarField;
+
+struct Frost {}
+
+impl Frost {
+    pub fn commit() {
+
+    }
+
+    pub fn signature_share() {
+
+    }
+
+    pub fn signature_aggregate() {
+    
+    }
+
+    pub fn verify() {
+
+    }
+}
+
+pub fn sign_signature_share(secret_share: ScalarField, challenge: ScalarField) {
+
+}
