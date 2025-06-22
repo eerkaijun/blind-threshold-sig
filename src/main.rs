7a6eb16c230877a94dd3845e881d3b77916d9f48
bing <spiralladder@keemail.me>
2025-06-22 10:17:52 +0200

    main: descwibe flow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 //! The main function contains a naive end-to-end flow of the FROST protocol, with the tweaks
-//! suggested in this gist to allow for blind signatures from a collaborative custodian. This
+//! suggested in this [gist](https://gist.github.com/nickfarrow/4be776782bce0c12cca523cbc203fb9d/) to allow for blind signatures from a collaborative custodian. This
 //! enables the user of the custodian to protect their own privacy as well as possibly have the
 //! option of switching away from this custodian at their own discretion.
-use ark_ed25519::Fr as ScalarField;
+//!
+//! For simplicity, we make no distinction of a 'collaborative custodian' in our implementation;
+//! we simply assign certain signers during setup as 'blind signers'.
+//!
+//! The rough flow goes like this, in steps:
+//!
+//! 1. At the start of the protocol, a secret key is generated and shared among signers.
+//!    This can either be done through a trusted dealer or a distributed key generation mechanism.
+//!    In this step, we also set a few signers above the `threshold` to be 'blind signers' as
+//!    suggested in the
+//!    [draft](https://gist.github.com/nickfarrow/4be776782bce0c12cca523cbc203fb9d?permalink_comment_id=4676206#gistcomment-4676206) instead of doing a key re-distribution and enrolment process for
+//!    simplicity.
+//!
+//! 2. In the [RFC](https://www.rfc-editor.org/rfc/rfc9591.html), a 'Coordinator' is chosen in the protocol, responsible for various group
+//!    actions such as aggregating signatures. In our simple example, the Coordinator is just
+//!    one of the signers.
+//!
+//! 3. Each signer generates a hiding and a binding nonce respectively and sends their commitments
+//!    to the Coordinator. The Coordinator collects these commitments and computes the
+//!    binding factors for all signers.
+//!
+//! 4.  Each signer generates a signature share using its secret key share, nonces and binding
+//!     factor.
+//!
+//! 5. The Coordinator aggregates these signature shares to produce a signature.
+//!
+//! 6. The Coordinator verifies the signature.
 
 use crate::{
     frost::Frost,
@@ -12,6 +38,7 @@ use crate::{
     },
     schnorr::SchnorrSignature,
 };
+use ark_ed25519::Fr as ScalarField;
 
 pub mod ciphersuite;
 pub mod frost;
